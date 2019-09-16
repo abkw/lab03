874662f4dbf1d96f3315d92439928bbf4c725e2b
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-16 16:28:46 +0200

    removing comments from dijkstra function

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -11,8 +11,8 @@
 #' wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
 #' v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
 #' w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-#'dijkstra(wiki_graph, 1)
-#'dijkstra(wiki_graph, 3)
+#' dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 3)
 #'
 
 dijkstra <- function(graph, init_node) {
@@ -34,7 +34,3 @@ dijkstra <- function(graph, init_node) {
   return(temp1)
 }
 
-#This is the graph code
-# adjm1<-matrix(sample(0:1,100,replace=TRUE,prob=c(0.9,01)),nc=6);
-# g1<-graph.adjacency(adjm1);
-# plot(g1)
