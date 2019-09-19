d49f606dad3964956ae2896e9aadf5ac80443bec
Mudith Chathuranga <mudithchathuranga@gmail.com>
2019-09-19 11:50:29 +0200

    Added again removed wiki_graph

---
+++ R/wiki_graph.R
@@ -0,0 +1,11 @@
+#' A dataset called wiki_graph used to check the output of dijkstra algorithm.
+#'
+#' @format A data frame with 54 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{A vector inside the wiki_graph dataframe containing the first edge.}
+#'   \item{v2}{A vector inside the wiki_graph dataframe containing the second edge.}
+#'   \item{w}{A vector inside the wiki_graph dataframe containing the weight between different edges.}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Graph/}
+"wiki_graph"
