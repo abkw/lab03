2a38777fbc07bca63aca8bd3680b5085b6605a5d
Mohammed Bakheet <m.yousify@gmail.com>
2019-10-01 23:33:40 +0200

    everything is fine

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -56,29 +56,5 @@ dijkstra <- function(graph, init_node) {
   return(path)
 }
 
-# Got help from Dijkstra's algorithm :- Wikipedia Pseudocode
-
-  # function Dijkstra(Graph, source):
-  #     create vertex set Q
-  #
-  #     for each vertex v in Graph:
-  #         dist[v] ← INFINITY
-  #         prev[v] ← UNDEFINED
-  #         add v to Q
-  #     dist[source] ← 0
-  #
-  #     while Q is not empty:
-  #           u ← vertex in Q with min dist[u]
-  #
-  #         remove u from Q
-  #
-  #         for each neighbor v of u:           // only v that are still in Q
-  #             alt ← dist[u] + length(u, v)
-  #             if alt < dist[v]:
-  #                   dist[v] ← alt
-  #                 prev[v] ← u
-  #
-  #     return dist[], prev[]
-
 
 
